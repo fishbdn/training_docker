20cb3fe90ca2e8c1dfeb02fe07d1876249d515ba
fishbdn <fishbdn@users.noreply.github.com>
2016-10-13 19:27:36 +0200

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	
 	public static void main(String[] args) {
-		System.err.println("Hello World! Conteneur Java");
+		System.err.println("Hello World! Conteneur Java de la branche 2");
 	}
 }
